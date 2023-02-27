43a97629a5d5d5289616381ae2890bdea62cc826
Yamato Baba <baba@infocity.co.jp>
2023-02-27 17:21:37 +0900

    add:initial commit

---
+++ android/app/src/main/kotlin/com/example/fitness_for_me/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fitness_for_me
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  DateTime _focused = DateTime.now();
+  DateTime? _selected;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: TableCalendar(
+          firstDay: DateTime.utc(2023,2,27),
+          lastDay: DateTime.utc(2025,12,31),
+          selectedDayPredicate: (day) {
+            return isSameDay(_selected, day);
+          },
+          onDaySelected: (selected, focused){
+            if (!isSameDay(_selected, selected)){
+              setState(() {
+                _selected = selected;
+                _focused = focused;
+              });
+            }
+          },
+          focusedDay: _focused,
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
